3eb45f3d348b2b3740064bdeb6bc0175aa8924c1
botwitha4 <143601652+botwitha4@users.noreply.github.com>
2023-12-19 07:39:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+PICS = (environ.get('PICS' ,'https://graph.org/file/2665872b5e93957a052a0.jpg https://graph.org/file/3642fff656457d5fb92e3.jpg https://graph.org/file/acd8052796e6d4f75fba9.jpg https://graph.org/file/ba3ced51152d736c47f05.jpg https://graph.org/file/64619d9f6868bd05377bd.jpg https://graph.org/file/e090475aad03830257f18.jpg https://graph.org/file/652059d09b04473ea47bd.jpg https://graph.org/file/5492d4eb3a504083da325.jpg')).split()
 UPTIME = time.time()
 
 # Admins, Channels & Users
